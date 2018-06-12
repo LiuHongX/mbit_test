feaa2ca3827e866a4369588b2b2ff83def510cec
zhyt <1628472468@qq.com>
2018-06-12 09:35:02 +0800

    09:34

--- enums.d.ts
+++ enums.d.ts
@@ -47,7 +47,7 @@
     NUM8 = 0x19,
     NUM9 = 0x1A,
     }
-declare namespace monkeybot {
+declare namespace monkeybot_遥控 {
 }
 
 // Auto-generated. Do not edit. Really.
--- shims.d.ts
+++ shims.d.ts
@@ -4,20 +4,20 @@
 
     //% color=50 weight=19
     //% icon="\uf1eb"
-declare namespace monkeybot {
+declare namespace monkeybot_遥控 {
 
     /**
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=monkeybot_::onPressEvent
+    //% block="on |%btn| button pressed" shim=monkeybot_遥控::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=monkeybot_::init
+    //% block="connect ir receiver to %pin" shim=monkeybot_遥控::init
     function init(pin: Pins): void;
 }
 
